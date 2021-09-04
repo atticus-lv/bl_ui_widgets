975596062982f70bb390c5420b9a38b397072e6b
MMMrqs <88054992+mmmrqs@users.noreply.github.com>
2021-09-04 20:15:48 -0300

    1.0.0: Refactored init file

--- __init__.py
+++ __init__.py
@@ -1,38 +1,102 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+#--- ### Header
 bl_info = {
     "name": "BL UI Widgets",
     "description": "UI Widgets to draw in the 3D view",
-    "author": "Jayanam",
-    "version": (0, 6, 4, 2),
-    "blender": (2, 80, 0),
-    "location": "View3D",
-    "category": "Object"}
+    "author": "Marcelo M. Marques (fork of Jayanam's original project)",
+    "version": (1, 0, 0),
+    "blender": (2, 80, 75),
+    "location": "View3D > side panel ([N]), [BL_UI_Widget] tab",
+    "support": "COMMUNITY",
+    "category": "3D View",
+    "warning": "Version numbering diverges from Jayanam's original project",
+    "doc_url": "https://github.com/mmmrqs/bl_ui_widgets",
+    "tracker_url": "https://github.com/mmmrqs/bl_ui_widgets/issues"
+    }    
 
-# Blender imports
+#--- ### Change log
+
+#v1.0.0 (09.01.2021) - by Marcelo M. Marques 
+#Added: initial creation
+
+#--- ### Imports
 import bpy
+import sys
+import importlib
 
 from bpy.props import *
 
-from . drag_panel_op import DP_OT_draw_operator
+modulesFullNames = {}
 
-addon_keymaps = []
+modulesNames = [
+                'prefs',
+                'bl_ui_draw_op',
+                'bl_ui_widget',
+                'bl_ui_label',
+                'bl_ui_patch',
+                'bl_ui_button',
+                'bl_ui_checkbox',
+                'bl_ui_textbox',
+                'bl_ui_slider',
+                'bl_ui_tooltip',
+                'bl_ui_drag_panel',
+                'demo_panel_op',
+                'bl_ui_widget_demo',
+               ]
 
+for currentModuleName in modulesNames:
+    if 'DEBUG_MODE' in sys.argv:
+        modulesFullNames[currentModuleName] = ('{}'.format(currentModuleName))
+    else:
+        modulesFullNames[currentModuleName] = ('{}.{}'.format(__name__, currentModuleName))
+
+if 'DEBUG_MODE' in sys.argv:
+    import os
+    import time
+    os.system("cls")
+    timestr = time.strftime("%Y-%m-%d %H:%M:%S")
+    print('---------------------------------------')
+    print('-------------- RESTART ----------------')
+    print('---------------------------------------')
+    print(timestr, __name__ + ": registered")
+    print()
+    sys.argv.remove('DEBUG_MODE')
+ 
+for currentModuleFullName in modulesFullNames.values():
+    if currentModuleFullName in sys.modules:
+        importlib.reload(sys.modules[currentModuleFullName])
+    else:
+        globals()[currentModuleFullName] = importlib.import_module(currentModuleFullName)
+        setattr(globals()[currentModuleFullName], 'modulesNames', modulesFullNames)
+ 
 def register():
-    
-    bpy.utils.register_class(DP_OT_draw_operator)
-    kcfg = bpy.context.window_manager.keyconfigs.addon
-    if kcfg:
-        km = kcfg.keymaps.new(name='3D View', space_type='VIEW_3D')
-   
-        kmi = km.keymap_items.new("object.dp_ot_draw_operator", 'F', 'PRESS', shift=True, ctrl=True)
-        
-        addon_keymaps.append((km, kmi))
-   
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
+
 def unregister():
-    for km, kmi in addon_keymaps:
-        km.keymap_items.remove(kmi)
-    addon_keymaps.clear()
-   
-    bpy.utils.unregister_class(DP_OT_draw_operator)
-    
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+ 
 if __name__ == "__main__":
     register()
